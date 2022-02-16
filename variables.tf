7efc80cf80167acd377833bb4cd02673177f9095
wcollins <william.levi.collins@gmail.com>
2022-02-16 09:31:16 -0500

    refactor: Modify billing tag variable type

--- variables.tf
+++ variables.tf
@@ -31,9 +31,10 @@ variable "subnets" {
   default     = [{}]
 }
 
-variable "billing_tag" {
-  description = "Alkira - billing tag"
-  type        = string
+variable "billing_tags" {
+  description = "List of billing tag names to apply to connector"
+  type        = list(string)
+  default     = []
 }
 
 variable "credential" {
